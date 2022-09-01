7b54c5b170377355f3943fb52088af4de0310b51
Vldadimir Ilin <bettyboop200195@gmail.com>
2022-09-01 17:59:53 +0300

    add create static: audio, img ; add search and lisen track

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { TrackModule } from "./track/track.module";
+import { FileModule } from './file/file.module';
+import { ServeStaticModule } from "@nestjs/serve-static";
+import * as path from 'path'
 
 @Module({
-imports: [
-    MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.hmxier4.mongodb.net/music-platform?retryWrites=true&w=majority'),
-    TrackModule
-]
+    imports: [
+        ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static') }),
+        MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.hmxier4.mongodb.net/music-platform?retryWrites=true&w=majority'),
+        TrackModule,
+        FileModule
+    ]
 })
 export class AppModule {
 
